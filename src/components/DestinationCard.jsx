49d21a3ec5278494a3bbad72751d8cb1e6b11ec9
Charlie Lang <charlie.lang@ucalgary.ca>
2024-11-18 16:35:16 -0700

    Refactor DestinationCard to use Link component for navigation and add styles for destination card links

--- src/components/DestinationCard.jsx
+++ src/components/DestinationCard.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import './DestinationCard.css';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { createSearchParams } from 'react-router-dom';
 
 
 const DestinationCard = ({ image, city, country }) => {
-    const navigate = useNavigate();
-
-    const handleClick = () => {
-        navigate({
-            pathname: "/find-events",
-            search: createSearchParams({
-                city: city,
-                country: country
-            }).toString()
-        });
-    };
   return (
-    <div className="destination-card" onClick={handleClick}>
+    <Link
+      to={{
+        pathname: "/find-events",
+        search: createSearchParams({
+            city: city,
+            country: country
+        }).toString()
+    }}
+    className="destination-card-link"
+    >
+    <div className="destination-card">
       <div className="destination-image" style={{ backgroundImage: `url(${image})` }}></div>
       <div className="destination-info">
         <h3 className="destination-city">{city}</h3>
         <p className="destination-country">{country}</p>
       </div>
     </div>
+    </Link>
   );
 };
 
