88b730cd38d9e07a38cd3464e5041cb6deca88a1
anilmawji <mawjianil1@gmail.com>
2024-12-02 10:32:30 -0700

    fix dropdown styling bugs

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -2,6 +2,21 @@ import styles from './DropDown.module.css';
 import { useEffect, useRef, useState } from 'react';
 import useBoolean from '../hooks/UseBoolean';
 
+const ArrowHeadDown = ({ className }) => {
+  return (
+    <svg className={className} viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
+      <g id="Layer_2" dataName="Layer 2">
+        <g id="invisible_box" dataName="invisible box">
+          <rect width="48" height="48" fill="none"/>
+        </g>
+        <g id="Q3_icons" dataName="Q3 icons">
+          <path d="M10.6,19.5l12,11.9a1.9,1.9,0,0,0,2.8,0l12-11.9A2,2,0,0,0,36,16H12a2,2,0,0,0-1.4,3.5Z"/>
+        </g>
+      </g>
+    </svg>
+  );
+}
+
 const DropDown = ({ dropdownOptions, placeholderText, onSelect }) => {
   const { value: isDropdownOpen, toggle: toggleDropdown, setFalse: closeDropdown } = useBoolean(false);
   const buttonRef = useRef(null);
@@ -33,7 +48,7 @@ const DropDown = ({ dropdownOptions, placeholderText, onSelect }) => {
         <button className={styles.toggle}>
           {selectedItem || placeholderText}
         </button>
-        <p className={styles.arrow}>⮟</p>
+        <ArrowHeadDown className={styles.arrow} />
       </div>
       <div className={`${styles.options} ${isDropdownOpen ? styles.visible : ''}`}>
         {dropdownOptions.map((option, i) => {
